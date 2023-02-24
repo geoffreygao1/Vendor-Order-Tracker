9946219dfee30d7ab4df0b5cc86e87dcb5c9edf1
Geoffrey Gao <geoffreygao1@gmail.com>
2023-02-24 15:07:09 -0800

    Added show vendor page

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -21,12 +21,22 @@ namespace Store.Controllers
       return View();
     }
 
-
     [HttpPost("/vendors")]
     public ActionResult Create(string inputName, string inputDescription)
     {
       Vendor newVendor = new Vendor(inputName, inputDescription);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
     }
   }
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -22,6 +22,10 @@ public class Vendor
   {
     return _instances;
   }
+  public static Vendor Find(int searchId)
+  {
+    return _instances[searchId - 1];
+  }
 
   public static void ClearAll()
   {
