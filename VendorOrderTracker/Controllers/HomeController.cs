5bede929d55b0ae2653dab01862b6bc3b8fed185
Geoffrey Gao <geoffreygao1@gmail.com>
2023-02-24 06:08:34 -0800

    Initialize project directories

---
+++ VendorOrderTracker/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectName.Controllers
+{
+  public class HomeController : Controller
+  {
+    //[Route("path")]
+    //public string Hello() { return "Hello friend!"; }
+
+    // [Route("/")]
+    // public ActionResult ModelName()
+    // {
+    //   //action
+    // }
+
+  }
+}
