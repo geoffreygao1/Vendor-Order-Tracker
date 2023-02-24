f6599cfc77866616b677da33ebf5b39fb40fca7d
Geoffrey Gao <geoffreygao1@gmail.com>
2023-02-24 06:47:47 -0800

    Created Order Class Frameork

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
+
 namespace Store.Models;
 
 public class Order
 {
-  public Order()
-  {
+  public string Title { get; set; }
+  public string Description { get; set; }
+  public string Price { get; set; }
+  public string Date { get; set; }
+  public bool IsPaid { get; set; }
+  public int Id { get; }
+  private static List<Order> _instances = new List<Order> { };
 
+  public Order(string inputName, string inputDescription)
+  {
+    _instances.Add(this);
+    Id = _instances.Count;
+  }
+
+  public static void ClearAll()
+  {
+    _instances.Clear();
   }
 }
+
